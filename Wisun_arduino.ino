5580cd221b1e6b380374adf85ff85ffc71bc2403
vaibhavnaware01 <66352097+vaibhavnaware01@users.noreply.github.com>
2022-07-16 17:21:49 +0530

    reset start issue fixed

--- Wisun_arduino.ino
+++ Wisun_arduino.ino
@@ -5,24 +5,55 @@
 const int zeroCrossPin  = 2;
 const int acdPin  = 3;
 int MIN_POWER  = 21;
-int MAX_POWER  = 36;
+int MAX_POWER  = 45;
 //Variables
 int power  = 0;
+String str="";
 //Objects
-SoftwareSerial mySerial(10, 11);//rx,tx
+SoftwareSerial mySerial(7, 8);//rx,tx
 dimmerLamp acd(acdPin);
-void setup(){
 
-Serial.println("sending connect command");
-pinMode(13, OUTPUT);
+void setup(){
+pinMode(LED_BUILTIN, OUTPUT);
+pinMode(A0, OUTPUT);
 Serial.begin(9600);
+Serial.println("sending connect command");
 Serial.println(F("Initialize System"));
 acd.begin(NORMAL_MODE, ON);
 mySerial.begin(9600);
+mySerial.println("wisun connect");
+ digitalWrite(LED_BUILTIN, HIGH);   
+ delay(5000);                       
+ digitalWrite(LED_BUILTIN, LOW);   
+
+while(mySerial.available())
+ {
+ digitalWrite(LED_BUILTIN, HIGH);   
+ delay(1000);                       
+ digitalWrite(LED_BUILTIN, LOW);   
+ delay(100); 
+str=mySerial.readStringUntil('\n');
+ Serial.println(str);
+ Serial.println(str.substring(1,3));
+ delay(5);
+  if(str.substring(1,3)=="Fa")
+  {
+   
+ digitalWrite(LED_BUILTIN, HIGH);   
+ delay(1000);                       
+ digitalWrite(LED_BUILTIN, LOW);   
+ delay(1000); 
+  break;
+  }
   mySerial.println("wisun connect");
-}
+
+    }
+  
+  }
+  
+  
 void loop(){
- String str="";
+ 
  while(mySerial.available())
  {
  str=mySerial.readStringUntil('\n');
@@ -35,14 +66,14 @@ void loop(){
   delay(500);
   mySerial.println("wisun udp_server 5001");
  } 
- if(str.substring(1,3)=="on")
+ if(str.substring(0,2)=="on")
  {
-  acd.setPower(MAX_POWER);
+  digitalWrite(A0,LOW);
   //Serial.println("on");
  }
-  if(str.substring(1,3)=="of")
+  if(str.substring(0,2)=="of")
  {
-  acd.setPower(0);
+  digitalWrite(A0,HIGH);
   //Serial.println("off");
  }
  if(str.substring(1,4)=="dim")
