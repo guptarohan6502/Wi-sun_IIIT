f244160000e35215fde93f553bba5d089b5d8fcb
vaibhavnaware01 <66352097+vaibhavnaware01@users.noreply.github.com>
2022-07-20 17:56:17 +0530

    Update Wisun_arduino.ino

--- Wisun_arduino/Wisun_arduino.ino
+++ Wisun_arduino/Wisun_arduino.ino
@@ -1,57 +1,60 @@
-//Libraries
 #include <SoftwareSerial.h>
 #include <RBDdimmer.h>//https://github.com/RobotDynOfficial/RBDDimmer
 //Parameters
-const int zeroCrossPin  = 2;
-const int acdPin  = 3;
-int MIN_POWER  = 21;
-int MAX_POWER  = 36;
+const int zeroCrossPin = 2;
+const int acdPin = 3;
+int MIN_POWER = 21;
+int MAX_POWER = 45;
 //Variables
-int power  = 0;
+int power = 0;
 //Objects
-SoftwareSerial mySerial(10, 11);//rx,tx
+SoftwareSerial mySerial(7, 8);//rx,tx
 dimmerLamp acd(acdPin);
 void setup(){
-
-Serial.println("sending connect command");
-pinMode(13, OUTPUT);
+delay(5000);
 Serial.begin(9600);
+mySerial.begin(9600);
+pinMode(13, OUTPUT);
+Serial.println("sending connect command");
 Serial.println(F("Initialize System"));
 acd.begin(NORMAL_MODE, ON);
-mySerial.begin(9600);
-  mySerial.println("wisun connect");
+mySerial.println("wisun connect");
 }
 void loop(){
- String str="";
- while(mySerial.available())
- {
- str=mySerial.readStringUntil('\n');
- Serial.println(str);
- Serial.println(str.substring(1,3));
- delay(5);
- if(str.substring(1,13)=="IPv6 address")
- {
-  Serial.println("working");
-  delay(500);
-  mySerial.println("wisun udp_server 5001");
- } 
- if(str.substring(1,3)=="on")
- {
-  acd.setPower(MAX_POWER);
-  //Serial.println("on");
- }
-  if(str.substring(1,3)=="of")
- {
-  acd.setPower(0);
-  //Serial.println("off");
- }
- if(str.substring(1,4)=="dim")
- {
-  acd.setPower(MIN_POWER);
-  //Serial.println("dim");
- }
-  }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
-  }
+String str="";
+while(mySerial.available())
+{
+str=mySerial.readStringUntil('\n');
+Serial.println(str);
+Serial.println(str.substring(1,3));
+delay(5);
+if(str.substring(1,13)=="IPv6 address")
+{
+Serial.println("working");
+//delay(500);
+mySerial.println("wisun udp_server 5001");
+//delay(1000);
+mySerial.println("wisun udp_client fd12:3456::1 5005");
+}
+if(str.substring(1,3)=="on")
+{
+acd.setPower(MAX_POWER);
+mySerial.println("wisun socket_write 4 \"its on!\"");
+Serial.println("on");
+}
+if(str.substring(1,3)=="off")
+{
+acd.setPower(0);
+Serial.println("off");
+mySerial.println("wisun socket_write 4 \"its off!\"");
+}
+if(str.substring(1,4)=="dim")
+{
+acd.setPower(MIN_POWER);
+//Serial.println("dim");
+}
+}
+if (Serial.available()) {
+mySerial.write(Serial.read());
+}
 }
