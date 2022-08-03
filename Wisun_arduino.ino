ef232dbfd4550c6562b31a5a1eb981f40aaf5d83
vaibhavnaware01 <66352097+vaibhavnaware01@users.noreply.github.com>
2022-08-03 14:09:35 +0530

    added individual node control commands

--- Wisun_arduino.ino
+++ Wisun_arduino.ino
@@ -1,79 +1,66 @@
-//Libraries
 #include <SoftwareSerial.h>
-#include <RBDdimmer.h>//https://github.com/RobotDynOfficial/RBDDimmer
-//Parameters
-const int zeroCrossPin  = 2;
-const int acdPin  = 3;
-int MIN_POWER  = 21;
-int MAX_POWER  = 45;
-//Variables
-int power  = 0;
-String str="";
-//Objects
+const int RelayPin = A3;
 SoftwareSerial mySerial(7, 8);//rx,tx
-dimmerLamp acd(acdPin);
-
-void setup(){
+void setup()
+{
 delay(5000);
-pinMode(A0, OUTPUT);
-Serial.begin(9600);
-Serial.println("sending connect command");
-Serial.println(F("Initialize System"));
-acd.begin(NORMAL_MODE, ON);
+//Serial.begin(9600);
 mySerial.begin(9600);
+pinMode(13, OUTPUT);
+pinMode(LED_BUILTIN, OUTPUT);
+pinMode(RelayPin, OUTPUT);
+digitalWrite(RelayPin, HIGH);
+//Serial.println("sending connect command");
+//Serial.println(F("Initialize System"));
 mySerial.println("wisun connect");
-
+}
+void loop()
+{
+String str="";
 while(mySerial.available())
- {
-
+{
 str=mySerial.readStringUntil('\n');
- Serial.println(str);
- Serial.println(str.substring(1,3));
- delay(5);
-  if(str.substring(1,3)=="Fa")
-  {
-   
-
-  break;
-  }
-  mySerial.println("wisun connect");
-
-    }
-  
-  }
-  
-  
-void loop(){
- 
- while(mySerial.available())
- {
- str=mySerial.readStringUntil('\n');
- Serial.println(str);
- Serial.println(str.substring(1,3));
- delay(5);
- if(str.substring(1,13)=="IPv6 address")
- {
-  Serial.println("working");
-  delay(500);
-  mySerial.println("wisun udp_server 5001");
- } 
- if(str.substring(0,2)=="on")
- {
-  digitalWrite(A0,LOW);
-  //Serial.println("on");
- }
-  if(str.substring(0,2)=="of")
- {
-  digitalWrite(A0,HIGH);
-  //Serial.println("off");
- }
- if(str.substring(1,4)=="dim")
- {
-  acd.setPower(MIN_POWER);
-  //Serial.println("dim");
- }
-  }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
-  }
+//Serial.println(str);
+//Serial.println(str.substring(1,3));
+delay(5);
+if(str.substring(1,13)=="IPv6 address")
+{
+//Serial.println("working");
+mySerial.println("wisun udp_server 5001");
+mySerial.println("wisun udp_client fd12:3456::1 5005");
+}
+if(str.substring(1,6)=="allon")
+{
+//acd.setPower(MAX_POWER);
+digitalWrite(RelayPin,LOW);
+digitalWrite(LED_BUILTIN, HIGH);
+mySerial.println("wisun socket_write 4 \"Node 4 is on\"");
+//Serial.println("on");
+}
+if(str.substring(1,7)=="alloff")
+{
+digitalWrite(RelayPin,HIGH);
+digitalWrite(LED_BUILTIN, LOW);
+mySerial.println("wisun socket_write 4 \"Node 4 is off!\"");
+//Serial.println("off");
+}
+if(str.substring(1,4)=="on4")
+{
+digitalWrite(RelayPin,LOW);
+digitalWrite(LED_BUILTIN, HIGH);
+mySerial.println("wisun socket_write 4 \"Node 4 is on!\"");
+//Serial.println("off");
+}
+if(str.substring(1,5)=="off4")
+{
+digitalWrite(RelayPin,HIGH);
+digitalWrite(LED_BUILTIN, LOW);
+mySerial.println("wisun socket_write 4 \"Node 4 is off!\"");
+//Serial.println("off");
+}
+}
+if (Serial.available()) 
+{ 
+mySerial.write(Serial.read());
+}
 }
