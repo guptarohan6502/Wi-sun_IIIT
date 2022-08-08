c08b5c3c8703f3064267385e567ba8e084d21e8a
vaibhavnaware01 <66352097+vaibhavnaware01@users.noreply.github.com>
2022-08-08 16:29:41 +0530

    Status thing is done

--- Wisun_arduino.ino
+++ Wisun_arduino.ino
@@ -1,17 +1,19 @@
 #include <SoftwareSerial.h>
-const int RelayPin = A3;
+const int RelayPin = A0;
 SoftwareSerial mySerial(7, 8);//rx,tx
+int relayState = 0;      // variable to store the read value
+
 void setup()
 {
 delay(5000);
-//Serial.begin(9600);
+Serial.begin(9600);
 mySerial.begin(9600);
-pinMode(13, OUTPUT);
+//pinMode(13, OUTPUT);
 pinMode(LED_BUILTIN, OUTPUT);
 pinMode(RelayPin, OUTPUT);
 digitalWrite(RelayPin, HIGH);
-//Serial.println("sending connect command");
-//Serial.println(F("Initialize System"));
+Serial.println("sending connect command");
+Serial.println(F("Initialize System"));
 mySerial.println("wisun connect");
 }
 void loop()
@@ -20,12 +22,12 @@ String str="";
 while(mySerial.available())
 {
 str=mySerial.readStringUntil('\n');
-//Serial.println(str);
-//Serial.println(str.substring(1,3));
+Serial.println(str);
+Serial.println(str.substring(1,3));
 delay(5);
 if(str.substring(1,13)=="IPv6 address")
 {
-//Serial.println("working");
+Serial.println("working");
 mySerial.println("wisun udp_server 5001");
 mySerial.println("wisun udp_client fd12:3456::1 5005");
 }
@@ -34,29 +36,41 @@ if(str.substring(1,6)=="allon")
 //acd.setPower(MAX_POWER);
 digitalWrite(RelayPin,LOW);
 digitalWrite(LED_BUILTIN, HIGH);
-mySerial.println("wisun socket_write 4 \"Node 4 is on\"");
-//Serial.println("on");
+mySerial.println("wisun socket_write 4 \"Node 6 is on\"");
+relayState = digitalRead(RelayPin);
+Serial.println("on");
 }
 if(str.substring(1,7)=="alloff")
 {
 digitalWrite(RelayPin,HIGH);
 digitalWrite(LED_BUILTIN, LOW);
-mySerial.println("wisun socket_write 4 \"Node 4 is off!\"");
+mySerial.println("wisun socket_write 4 \"Node 6 is off!\"");
+relayState = digitalRead(RelayPin);
 //Serial.println("off");
 }
-if(str.substring(1,4)=="on4")
+if(str.substring(1,4)=="on6")
 {
 digitalWrite(RelayPin,LOW);
 digitalWrite(LED_BUILTIN, HIGH);
-mySerial.println("wisun socket_write 4 \"Node 4 is on!\"");
-//Serial.println("off");
+mySerial.println("wisun socket_write 4 \"Node 6 is on!\"");
+relayState = digitalRead(RelayPin);
+
+Serial.println("off");
 }
-if(str.substring(1,5)=="off4")
+if(str.substring(1,5)=="off6")
 {
 digitalWrite(RelayPin,HIGH);
 digitalWrite(LED_BUILTIN, LOW);
-mySerial.println("wisun socket_write 4 \"Node 4 is off!\"");
-//Serial.println("off");
+mySerial.println("wisun socket_write 4 \"Node 6 is off!\"");
+relayState = digitalRead(RelayPin);
+Serial.println("off");
+}
+if(str.substring(1,5)=="sts6")
+{
+  if (relayState == HIGH)
+mySerial.println("wisun socket_write 4 \"Node 6 is off!\"");
+ else
+mySerial.println("wisun socket_write 4 \"Node 6 is on!\"");
 }
 }
 if (Serial.available()) 
